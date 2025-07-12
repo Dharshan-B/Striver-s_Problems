dac441c66fbe8df9c6e66896ceec08c680de0df9
Dharshan B <dharshanb0611@gmail.com>
2025-07-12 17:23:27 +0530

    initial

--- Basic_Math_Questions/Count_Digits.java
+++ Basic_Math_Questions/Count_Digits.java
@@ -1,11 +1,16 @@
 public class Count_Digits{
   public static void main(String[] args){
-    int a = 12345;
-    int count =0;
-    while(a>0){
-      a = a/10;
-    count++;
-  }
-  System.out.print(count);
+    int a = 1234;
+    int count = 0;
+    if(a == 0){
+      System.out.println("1");
+    }
+    else{
+          while(a>0){
+          a = a/10;
+          count++;
+          }
+      System.out.print(count);
+    }
   }
 }
