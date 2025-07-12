a30d6055bde5be026bb97184b9ff9821ae2f8690
Dharshan B <dharshanb0611@gmail.com>
2025-07-12 18:01:22 +0530

    Reverse

--- Basic_Math_Questions/Reverse_digit.java
+++ Basic_Math_Questions/Reverse_digit.java
@@ -15,3 +15,28 @@ public class Reverse_digit {
     System.out.println(result);
   }
 }
+/*
+Optimized and Edge case Clearence
+ */
+/*
+Optimized and Edge case Clearence
+class Solution {
+    public int reverse(int x) {
+        boolean isNegative = x < 0;
+        x = Math.abs(x);
+        int resNum  = 0;
+        while(x>0){
+            int num = x%10;
+                if (resNum > Integer.MAX_VALUE / 10 || (resNum == Integer.MAX_VALUE / 10 && num > 7)) {
+                    return 0;
+                }
+                if (resNum < Integer.MIN_VALUE / 10 || (resNum == Integer.MIN_VALUE / 10 && num < -8)) {
+                    return 0;
+                }
+            resNum = (resNum*10)+num;
+            x=x/10;
+        }
+        return isNegative ? -resNum : resNum;    
+    }
+}
+ */
