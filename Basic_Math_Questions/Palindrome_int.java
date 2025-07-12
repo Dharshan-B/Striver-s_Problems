6fc2800c05d1ba6b4a18dcafd3dbf0ac186518c9
Dharshan B <dharshanb0611@gmail.com>
2025-07-12 19:55:08 +0530

    Palindrome

---
+++ Basic_Math_Questions/Palindrome_int.java
@@ -0,0 +1,22 @@
+public class Palindrome_int {
+  public static boolean palin(int x){
+    if(x<0) return false;
+    int original = x;
+    int Reversed = 0;
+
+    while(x > 0){
+      int num = x% 10 ;
+      Reversed = (Reversed * 10)+ num;
+      x = x/10;
+    }
+
+    if(original==Reversed)
+      return true;
+    else
+      return false;
+  }
+  public static void main(String[] args){
+    int x = 121;
+    System.out.println(palin(x));
+  }  
+}
