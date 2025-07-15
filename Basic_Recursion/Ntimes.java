e9cab446ffbb81a3396462634be71bcb2695747b
Dharshan B <dharshanb0611@gmail.com>
2025-07-15 13:50:33 +0530

    Recursion

---
+++ Basic_Recursion/Ntimes.java
@@ -0,0 +1,18 @@
+package Basic_Recursion;
+
+public class Ntimes {
+  public static int count(int n){
+    int count = 0;
+    for(int i = 0;i < n ; i++){
+      count++;
+      System.out.println(count);
+    }
+    return count(n);
+    
+  }
+  public static void main(String[] args){
+    int n = 12;
+    System.out.println(count(n));
+
+  }
+}
