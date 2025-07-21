1f7c4275a87d2ab045d6c7afd5afca931fe32a20
Dharshan B <dharshanb0611@gmail.com>
2025-07-21 18:27:30 +0530

    Coming back

--- 3_Basic_Math_Questions/Count_Digits.java
+++ 3_Basic_Math_Questions/Count_Digits.java
@@ -1,13 +1,14 @@
 public class Count_Digits{
   public static void main(String[] args){
-    int a = 1234;
+    int a = -1234;
+    int b = Math.abs(a);
     int count = 0;
-    if(a == 0){
+    if(b == 0){
       System.out.println("1");
     }
     else{
-          while(a>0){
-          a = a/10;
+          while(b>0){
+          b = b/10;
           count++;
           }
       System.out.print(count);
